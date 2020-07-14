8cdc346fcb543246d92b7247bbc5aac07bd81a74
Mqasim0 <muhammadqasim3991@gmail.com>
2020-07-15 01:12:44 +0500

    uplaoding files

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>Github Finder App</h1>
+      <p>Find guthub users through search box</p>
+      <p>version 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
